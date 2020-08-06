f7bbaac521183154fcf8fd94921e6a4b374e7e03
Maksym <maksym@digitalmaker.io>
2020-08-06 15:39:02 +0300

    Initial commit

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Container(
+            padding: EdgeInsets.all(8),
+            child: Text('Thes works!'),
+          );
+        },
+        itemCount: 10,
+      ),
+    );
+  }
+}
