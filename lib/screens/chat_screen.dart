ffc3824d12422f7b26a84883d3ea1b7c7f998a47
Maksym <maksym@digitalmaker.io>
2020-08-13 10:52:06 +0300

    added firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,21 +5,25 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8.0),
-          child: Text('This works'),
-        ),
-        itemCount: 10,
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/LKW6Aw1kDA2aEGn76BF8/messages')
+            .snapshots(),
+        builder: (ctx, snapshot) {
+          return ListView.builder(
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8.0),
+              child: Text('This works'),
+            ),
+            itemCount: 2,
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          Firestore.instance
-              .collection('chats/LKW6Aw1kDA2aEGn76BF8/messages')
-              .snapshots()
-              .listen((data) {
-            print(data);
+          Firestore.instance.collection('chats').snapshots().listen((data) {
+            print(data.documents);
           });
         },
       ),
