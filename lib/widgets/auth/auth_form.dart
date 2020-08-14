eb00841a99ca3adf87d4f2e8f0875b7fe745b811
A1doranM <vaselisk1997@gmail.com>
2020-08-14 13:37:41 +0300

    Added auth screen

---
+++ lib/widgets/auth/auth_form.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  @override
+  _AuthFormState createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Card(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Form(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email address',
+                  ),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Username'),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                RaisedButton(
+                  child: Text('Login'),
+                  onPressed: () {},
+                ),
+                FlatButton(
+                  child: Text('Create new account'),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      margin: EdgeInsets.all(20),
+    ));
+  }
+}
